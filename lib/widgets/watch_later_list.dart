e05eba6302844e971ab18d396c4ca03d2b7c3c1e
Sharifah <sharifah@besquare.com.my>
2022-08-11 14:34:15 +0800

    add to watch later

--- lib/widgets/watch_later_list.dart
+++ lib/widgets/watch_later_list.dart
@@ -14,7 +14,40 @@ class WatchLaterList extends StatelessWidget {
     const rowSpacer = SizedBox(width: 10);
 
     return SingleChildScrollView(
-      child: Container(),
+      child: SizedBox(
+        width: 100.0,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(
+              color: Colors.white.withOpacity(0.5),
+              width: 1,
+            ),
+          ),
+          elevation: 20,
+          shadowColor: Colors.purpleAccent,
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                // const Icon(
+                //   Icons.watch_later,
+                //   color: Colors.white,
+                //   size: 35,
+                // ),
+                Text(
+                  movieModel.title,
+                ),
+                // const Text(
+                //   'Duration',
+                //   style: TextStyle(fontWeight: FontWeight.bold),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
